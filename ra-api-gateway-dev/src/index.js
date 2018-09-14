85eb6aa8f8e556fb46c6314fb33d014088ef0955
Tolik Dem <tolik.demianov@gmail.com>
2018-09-14 19:22:47 +0300

    сделал серверную часть калькулятора по-минимуму

--- ra-api-gateway-dev/src/index.js
+++ ra-api-gateway-dev/src/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
+import url from 'url'; // for parsing GET
 import initializeDb from './db'
 import config from './config'
 import socket from './socket'
@@ -46,6 +47,42 @@ initializeDb(db => {
       __DEV__ ? `\nconfig: ${JSON.stringify(config, null, 2)}` : ''
     )
   })
+})
+
+/*
+В GET запросе сервер получает JSON вида {"+": [2,3]} где ключ это знак операции, массив из двух чисел это операнды
+Парсит JSON.
+Выполняет операцию над числами в зависимости от знака.
+Возвращает результат.
+*/
+app.server.on('request', function (req, res) {
+  const urlParsed = url.parse(req.url, true)
+  let answer = 'no answer';
+
+  if (urlParsed.pathname === '/' && urlParsed.query.data) {
+    const obj = JSON.parse(urlParsed.query.data)
+    let operator;
+    // определить оператор
+    Object.entries(obj).forEach(item => {
+      if ('*/+-'.indexOf(item[0]) !== -1) { operator = item[0] }
+    })
+    // выполнить операцию в зависимости от оператора
+    if (operator) {
+      switch (operator) {
+        case '*': answer = obj['*'][0] * obj['*'][1]; break;
+        case '/': answer = obj['/'][0] / obj['/'][1]; break;
+        case '+': answer = obj['+'][0] + obj['+'][1]; break;
+        case '-': answer = obj['-'][0] - obj['-'][1]; break;
+        default: 
+          answer = 'no answer'
+      }
+    } else {
+      answer = 'no operator';
+    }
+  } /* (urlParsed.pathname === '/' && urlParsed.query.data) */ else {
+    answer = 'no data'
+  }
+  res.end(String(answer))
 })
 
 export default app
